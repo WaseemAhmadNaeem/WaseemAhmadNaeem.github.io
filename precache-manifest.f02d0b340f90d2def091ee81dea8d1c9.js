8125fb4b2264c6dde03f7e324486eda132dcb17c
Waseem Ahmad Naeem <ahmadwaseem237@gmail.com>
2019-11-23 15:33:48 +0500

    first commit

---
+++ precache-manifest.f02d0b340f90d2def091ee81dea8d1c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43836208e91d897b82e01267b97ed10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c689742f242c704f8175",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "330202849a85cc919125",
+    "url": "/static/js/2.332540af.chunk.js"
+  },
+  {
+    "revision": "c689742f242c704f8175",
+    "url": "/static/js/main.080719fa.chunk.js"
+  },
+  {
+    "revision": "7b355e162d7d60d1f23d",
+    "url": "/static/js/runtime-main.fc6de12b.js"
+  }
+]);
